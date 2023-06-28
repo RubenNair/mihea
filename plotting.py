074caef23bb50cf765077395f6883cd62867e54f
RubenNair <ruben.k.nair@gmail.com>
2023-06-28 11:58:21 +0200

    change values of plotting for split objective

--- plotting.py
+++ plotting.py
@@ -12,9 +12,9 @@ x = [40, 80, 120, 160]
 #ys = [[4236, 14849, 33709, 58528], #[[19932, 61612, 137139, 217356],   # 0.25 
 #     [14058, 51816, 114000, 184206],    # 0.5  
 #     [29527, 101776, 225688, 355719]]    # 0.75
-ys = [[4272, 14849, 33709, 58528], #[[19932, 61612, 137139, 217356],   # 0.25 
-     [14003, 51816, 114000, 184206],    # 0.5  
-     [28955, 100925, 232334, 355719]]    # 0.75
+ys = [[4272, 14907, 34162, 58528], #[[19932, 61612, 137139, 217356],   # 0.25 
+     [14003, 50154, 118162, 187839],    # 0.5  
+     [28955, 100925, 232334, 359446]]    # 0.75
 
 plt.loglog(x, ys[0], 'ro-', label='f=0.25', color='red')
 plt.loglog(x, ys[1], 'ro-', label='f=0.5', color='green')
@@ -46,11 +46,11 @@ plt.show()
 # F3
 plt.figure()
 x = [40, 80, 120, 160]
-ys = [[-1, -1, -1, -1],   # 0.25  DIDN'T HAVE ANY VTR HITS
-     [1154892, 2498129, 518911, 889026],    # 0.5  
-     [39761, 130092, 275686, 469221]]    # 0.75
+ys = [[20431, 36527, 60976, 108917],   # 0.25  DIDN'T HAVE ANY VTR HITS
+     [23763, 66336, 136949, 236786],    # 0.5  
+     [39761, 130092, 275686, 478642]]    # 0.75
 
-#plt.loglog(x, ys[0], 'ro-', label='f=0.25', color='red')
+plt.loglog(x, ys[0], 'ro-', label='f=0.25', color='red')
 plt.loglog(x, ys[1], 'ro-', label='f=0.5', color='green')
 plt.loglog(x, ys[2], 'ro-', label='f=0.75', color='blue')
 plt.xticks(x)
@@ -61,19 +61,19 @@ plt.legend()
 plt.show()
 
 
-# F4
-plt.figure()
-x = [40, 80, 120, 160]
-ys = [[-1, -1, -1, -1],   # 0.25  DIDN'T HAVE ANY VTR HITS
-     [34013, 87202, 195221, 324518],    # 0.5  
-     [55594, 175926, 413803, 709015]]    # 0.75
-
-#plt.loglog(x, ys[0], 'ro-', label='f=0.25', color='red')
-plt.loglog(x, ys[1], 'ro-', label='f=0.5', color='green')
-plt.loglog(x, ys[2], 'ro-', label='f=0.75', color='blue')
-plt.xticks(x)
-plt.title('F4 TrapEllipse')
-plt.xlabel('Problem Size')
-plt.ylabel('Number of Evaluations')
-plt.legend()
-plt.show()
+## F4
+#plt.figure()
+#x = [40, 80, 120, 160]
+#ys = [[-1, -1, -1, -1],   # 0.25  DIDN'T HAVE ANY VTR HITS
+#     [34013, 87202, 195221, 324518],    # 0.5  
+#     [55594, 175926, 413803, 709015]]    # 0.75
+#
+##plt.loglog(x, ys[0], 'ro-', label='f=0.25', color='red')
+#plt.loglog(x, ys[1], 'ro-', label='f=0.5', color='green')
+#plt.loglog(x, ys[2], 'ro-', label='f=0.75', color='blue')
+#plt.xticks(x)
+#plt.title('F4 TrapEllipse')
+#plt.xlabel('Problem Size')
+#plt.ylabel('Number of Evaluations')
+#plt.legend()
+#plt.show()
