7fdaf825f5aabf668abca6e36905ae67f38d5471
RubenNair <ruben.k.nair@gmail.com>
2023-06-28 10:48:01 +0200

    Add command line arguments to python script

--- gomea/run_discrete.py
+++ gomea/run_discrete.py
@@ -1,7 +1,23 @@
 import gomea
-f = gomea.fitness.DeceptiveTrapFunction(60,trap_size=5)
+import sys
+
+n_vars = 60
+k = 5
+n_pop = 100
+if(len(sys.argv) == 2):
+    n_vars = int(sys.argv[1])
+elif(len(sys.argv) == 3):
+    n_vars = int(sys.argv[1])
+    k = int(sys.argv[2])
+elif(len(sys.argv) == 4):
+    n_vars = int(sys.argv[1])
+    k = int(sys.argv[2])
+    n_pop = int(sys.argv[3])
+
+print("Running discrete GOMEA with n_vars = {}, k = {}, n_pop = {}".format(n_vars,k,n_pop))
+f = gomea.fitness.DeceptiveTrapFunction(n_vars,trap_size=k)
 lm = gomea.linkage.LinkageTree()
-gd = gomea.DiscreteGOMEA(fitness=f,linkage_model=lm,max_number_of_populations=1,base_population_size=100)
+gd = gomea.DiscreteGOMEA(fitness=f,linkage_model=lm,max_number_of_populations=1,base_population_size=n_pop)
 out = gd.run()
 
 out.printAllStatistics()
